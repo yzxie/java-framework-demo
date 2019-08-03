53f0f5661f6f27d16e895e0e80779bb253b65e29
xieyizun <xieyz3@mail2.sysu.edu.cn>
2019-08-04 00:16:27 +0800

    spring ioc 容器的使用demo

---
+++ spring-demo/src/main/java/com/yzxie/demo/java/springdemo/context/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package com.yzxie.demo.java.springdemo.context;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author: xieyizun
+ * Version: 1.0
+ * Date: 2019-08-03
+ * Description:
+ **/
+@Configuration
+@ComponentScan
+public class ApplicationConfig {
+
+}
---
+++ spring-demo/src/main/java/com/yzxie/demo/java/springdemo/context/ObjectB.java
@@ -0,0 +1,22 @@
+package com.yzxie.demo.java.springdemo.context;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: xieyizun
+ * Version: 1.0
+ * Date: 2019-08-03
+ * Description:
+ **/
+@Component
+public class ObjectB {
+    private String name = "B";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
